6feae826c5c1801f49583637019a0a4da983281a
Kunapareddy Rajesh Babu <kunapareddyrajeshbabu@hotmail.com>
2016-11-30 23:31:24 +0530

    Login form and Controller with out DB, Still need to change form.

---
+++ MineralWaterPlant/Controllers/AccountController.cs
@@ -0,0 +1,222 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AccountController.cs" company="None">
+//     Copyright (c) Allow to distribute this code.
+// </copyright>
+// <author>Asma Khalid</author>
+//-----------------------------------------------------------------------
+
+namespace AppTracker.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Web;
+    using System.Web.Mvc;
+    using AodNetIntegration;
+    using AodNetIntegration.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.Owin.Security;
+
+    /// <summary>
+    /// Account controller class.
+    /// </summary>
+    public class AccountController : Controller
+    {
+        #region Private Properties
+
+        /// <summary>
+        /// Database Store property.
+        /// </summary>
+        private AdoNetIntegrationEntities databaseManager = new AdoNetIntegrationEntities();
+
+        #endregion
+
+        #region Default Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccountController" /> class.
+        /// </summary>
+        public AccountController()
+        {
+        }
+
+        #endregion
+
+        #region Login methods
+
+        /// <summary>
+        /// GET: /Account/Login
+        /// </summary>
+        /// <param name="returnUrl">Return URL parameter</param>
+        /// <returns>Return login view</returns>
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            try
+            {
+                // Verification.
+                if (this.Request.IsAuthenticated)
+                {
+                    // Info.
+                    return this.RedirectToLocal(returnUrl);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Info
+                Console.Write(ex);
+            }
+
+            // Info.
+            return this.View();
+        }
+
+        /// <summary>
+        /// POST: /Account/Login
+        /// </summary>
+        /// <param name="model">Model parameter</param>
+        /// <param name="returnUrl">Return URL parameter</param>
+        /// <returns>Return login view</returns>
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            try
+            {
+                // Verification.
+                if (ModelState.IsValid)
+                {
+                    // Initialization.
+                    var loginInfo = this.databaseManager.LoginByUsernamePassword(model.Username, model.Password).ToList();
+
+                    // Verification.
+                    if (loginInfo != null && loginInfo.Count() > 0)
+                    {
+                        // Initialization.
+                        var logindetails = loginInfo.First();
+
+                        // Login In.
+                        this.SignInUser(logindetails.username, false);
+
+                        // Info.
+                        return this.RedirectToLocal(returnUrl);
+                    }
+                    else
+                    {
+                        // Setting.
+                        ModelState.AddModelError(string.Empty, "Invalid username or password.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Info
+                Console.Write(ex);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return this.View(model);
+        }
+
+        #endregion
+
+        #region Log Out method.
+
+        /// <summary>
+        /// POST: /Account/LogOff
+        /// </summary>
+        /// <returns>Return log off action</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            try
+            {
+                // Setting.
+                var ctx = Request.GetOwinContext();
+                var authenticationManager = ctx.Authentication;
+
+                // Sign Out.
+                authenticationManager.SignOut();
+            }
+            catch (Exception ex)
+            {
+                // Info
+                throw ex;
+            }
+
+            // Info.
+            return this.RedirectToAction("Login", "Account");
+        }
+
+        #endregion
+
+        #region Helpers
+
+        #region Sign In method.
+
+        /// <summary>
+        /// Sign In User method.
+        /// </summary>
+        /// <param name="username">Username parameter.</param>
+        /// <param name="isPersistent">Is persistent parameter.</param>
+        private void SignInUser(string username, bool isPersistent)
+        {
+            // Initialization.
+            var claims = new List<Claim>();
+
+            try
+            {
+                // Setting
+                claims.Add(new Claim(ClaimTypes.Name, username));
+                var claimIdenties = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+                var ctx = Request.GetOwinContext();
+                var authenticationManager = ctx.Authentication;
+
+                // Sign In.
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, claimIdenties);
+            }
+            catch (Exception ex)
+            {
+                // Info
+                throw ex;
+            }
+        }
+
+        #endregion
+
+        #region Redirect to local method.
+
+        /// <summary>
+        /// Redirect to local method.
+        /// </summary>
+        /// <param name="returnUrl">Return URL parameter.</param>
+        /// <returns>Return redirection action</returns>
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            try
+            {
+                // Verification.
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    // Info.
+                    return this.Redirect(returnUrl);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Info
+                throw ex;
+            }
+
+            // Info.
+            return this.RedirectToAction("Index", "Home");
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
